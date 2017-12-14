d33250cc872a8be60b98f526a3ab994fa60edab9
Yomaira Escano <yjescano@gmail.com>
2017-12-14 04:10:54 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,8 @@
-CREATE TABLE bears (
-  id INTEGER PRIMARY KEY,
+CREATE TABLE bears 
+  (id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER,
   gender CHAR(1),
   color TEXT,
   temperament TEXT,
-  alive BOOLEAN
-);
+  alive BOOLEAN);
